632f9a992c7f858e4b95959a5e04b6fd051fa8f8
Alliah Gabrielle Galang <alliahgabrielle@gmail.com>
2026-06-18 17:32:18 +0800

    working intro

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 
-document.getElementById('intro-overlay').style.display = 'none';
-showCrossroads(); // remove to enable intro video and transition
+//document.getElementById('intro-overlay').style.display = 'none';
+//showCrossroads(); // remove to enable intro video and transition
 
 // ── Theme toggle ──
 const root = document.documentElement;
